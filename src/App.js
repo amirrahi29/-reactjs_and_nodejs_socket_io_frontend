f8e59623de45099c86bba4c0000c06c90f49dd34
Amir Rahi <amirrahi29@gmail.com>
2023-06-11 17:30:31 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import socketIo from "socket.io-client";
+import socketIo from 'socket.io-client';
 
-const ENDPOINT = "http://localhost:8000";
+const ENDPOINT = 'http://localhost:8000';
 let socket;
 
 const App = () => {
@@ -11,12 +11,18 @@ const App = () => {
   useEffect(() => {
     socket = socketIo(ENDPOINT, { transports: ['websocket'] });
 
-    // socket.on('chatMessage', (data) => {
-    //   setProduct(data.product);
-    // });
-
     socket.on('products', (data) => {
-      setProduct(data.product);
+      console.log(`products: ${JSON.stringify(data.products)}`);
+      setProduct(data.products);
+    });
+
+    socket.on('chatMessage', async () => {
+      try {
+        const response = await fetchProducts();
+        setProduct(response.products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     });
 
     return () => {
@@ -27,21 +33,44 @@ const App = () => {
 
   const handleSubmit = () => {
     socket.emit('chatMessage', { message: message, id: socket.id });
-    console.log(`product: ${JSON.stringify(product)}`);
+    setMessage('');
   };
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/products');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error('Error fetching products:', error);
+    }
+  };
+
+ const deleteProduct=async(id)=>{
+  socket.emit('deleteProduct', { product_id: id, id: socket.id });
+ }
 
   return (
     <>
       <div style={{ margin: 16 }}>
         <p>My Chat App</p>
         <hr />
-        <input type="text" value={message} placeholder='Enter your message' onChange={(e) => setMessage(e.target.value)} /><br /><br />
+        <input
+          type="text"
+          value={message}
+          placeholder="Enter your message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <br />
+        <br />
         <button onClick={handleSubmit}>Submit</button>
         <hr />
         <br />
         <div>
           {product.map((item) => (
-            <div key={item.id}>{item.title}</div>
+            <div key={item._id}>{item.title} &nbsp;&nbsp;&nbsp; 
+            <span onClick={()=>deleteProduct(item._id)} style={{color:'red',cursor:'pointer'}}>X</span></div>
+            
           ))}
         </div>
       </div>
